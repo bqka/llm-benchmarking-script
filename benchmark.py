4c63e0ed8d48cdd7aa1f339143a1220067c5365f
bqka <aditya2005mendiratta@gmail.com>
2025-06-26 23:44:38 +0530

    fixed output folder naming issue in gemini

--- benchmark.py
+++ benchmark.py
@@ -5,8 +5,8 @@ import json
 BATCH_SIZE = 5
 DATA_PATH = "human_evaluation_binary"
 
-gemini_model = "models/gemini-2.0-flash"
-output_dir = os.path.join("outputs", gemini_model)
+gemini_model = "gemini-2.0-flash"
+gemini_output_dir = os.path.join("outputs", gemini_model)
 
 ### RATE LIMITER
 with open("models.json", "r") as f:
@@ -16,12 +16,12 @@ rpm = next((m["rpm"] for m in model_config["gemini-models"] if m["name"] == gemi
 rate_limiter = RateLimiter(rpm)
 ###
 
-os.makedirs(output_dir, exist_ok=True)
+os.makedirs(gemini_output_dir, exist_ok=True)
 
 for file in os.listdir(DATA_PATH):
     print(f"Running on file {file}...")
     file_path = os.path.join(DATA_PATH, file)
-    gemini_output = os.path.join(output_dir, file)
+    gemini_output = os.path.join(gemini_output_dir, file)
 
     gemini_benchmark = GeminiBenchmark(gemini_model, BATCH_SIZE, file_path, rate_limiter)
     gemini_benchmark.run(gemini_output)
